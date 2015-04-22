a6c93b4f981068beb372dedcbf449d502ca1c4d7
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-04-21 18:15:46 -0700

    set up java 2d

--- src/com/troi/balloon/Engine.java
+++ src/com/troi/balloon/Engine.java
@@ -1,13 +1,23 @@
 package com.troi.balloon;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Engine {
 	
+	@SuppressWarnings("serial")
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Test");
+		frame.setContentPane(new JPanel() {
+			@Override
+			public void paintComponent(Graphics g) {
+				Graphics2D render = (Graphics2D) g;
+			}
+		});
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(800, 600));
 		frame.pack();
