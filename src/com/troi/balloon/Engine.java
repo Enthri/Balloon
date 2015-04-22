0d7a3e929e435231e936e7046b5dd1f13e707eb7
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-04-22 11:11:09 -0700

    experimetaton

--- src/com/troi/balloon/Engine.java
+++ src/com/troi/balloon/Engine.java
@@ -2,19 +2,28 @@ package com.troi.balloon;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 
-import javax.swing.JEditorPane;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
 
-import jsyntaxpane.DefaultSyntaxKit;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
+import org.fife.ui.rtextarea.RTextScrollPane;
 
 public class Engine {
 	
 	@SuppressWarnings("serial")
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Test");
+		final JFrame frame = new JFrame("Test");
 		frame.setContentPane(new JPanel() {
 			@Override
 			public void paintComponent(Graphics g) {
@@ -24,13 +33,59 @@ public class Engine {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setPreferredSize(new Dimension(800, 600));
 		frame.pack();
-		DefaultSyntaxKit.initKit();
-        final JEditorPane codeEditor = new JEditorPane();
-        JScrollPane scrPane = new JScrollPane(codeEditor);
-        scrPane.setPreferredSize(new Dimension(400,600));
-        codeEditor.setContentType("text/java");
-        codeEditor.setText("public class Test {\n\tpublic static void main(String[] args) {\n\t\t\n\t}\n}");
-		frame.add(scrPane);
+        final RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+        textArea.setCodeFoldingEnabled(true);
+        textArea.setText("public class Test {\n}");
+        RTextScrollPane sp = new RTextScrollPane(textArea);
+        frame.add(sp);
+        System.setProperty("java.home", "C:\\Program Files\\Java\\jdk1.7.0_71");
+        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
 		frame.setVisible(true);
+		final JFileChooser chooser = new JFileChooser();
+		frame.addMouseListener(new MouseListener(){
+
+
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if(chooser.showSaveDialog(frame) == JFileChooser.APPROVE_OPTION) {
+					PrintWriter writer;
+					try {
+						writer = new PrintWriter(chooser.getSelectedFile().toString(), "UTF-8");
+						for(String a : textArea.getText().split("\n")) writer.println(a);
+						writer.close();
+					} catch (FileNotFoundException | UnsupportedEncodingException e) {
+						e.printStackTrace();
+					}
+					compiler.run(null, null, null, chooser.getSelectedFile().toString());
+				}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+		
 	}
 }
