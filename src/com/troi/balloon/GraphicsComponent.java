034fa0c57747b535fd510b40ec9619b29d123fe7
alexander Patapoff <awpatapoff@gmail.com>
2015-04-24 18:26:49 -0700

    started drawing structure

--- src/com/troi/balloon/GraphicsComponent.java
+++ src/com/troi/balloon/GraphicsComponent.java
@@ -3,20 +3,35 @@ package com.troi.balloon;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 
 import javax.swing.JComponent;
 
 public class GraphicsComponent extends JComponent{
 	Graphics2D paint;
+	ArrayList<Panel> panels = new ArrayList<Panel>();
 	public void paintComponent(Graphics g)
 	{
 		this.paint = (Graphics2D) g;
+		for (int x = 0; x > panels.size()-1; x++)
+		{
+			if (panels.get(x).getRepaintValue() == true)
+			{
+				panels.get(x).repaint(paint);
+			}
+		}
+		
 	}
 	
 	public Graphics2D getGraphics()
 	{
 		return paint;
 	}
+	public void newPanel(Panel panel)
+	{
+		panels.add(panel);
+	}
+	
 	public void test()
 	{
 		Rectangle2D.Double x = new Rectangle2D.Double(10,10,10,10);
--- src/com/troi/balloon/Panel.java
+++ src/com/troi/balloon/Panel.java
@@ -1,6 +1,38 @@
 package com.troi.balloon;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.util.ArrayList;
+import java.awt.geom.Rectangle2D;
 public class Panel {
+	protected boolean repaintValue = false;
+	protected ArrayList<Object> objects = new ArrayList<Object>();
+	public Panel()
+	{
+		repaintValue = true;
+	}
+	public boolean getRepaintValue()
+	{
+		return repaintValue;
+	}
+	public void repaint(Graphics2D paint)
+	{
+		for (int x = 0; x < objects.size()-1; x++)
+		{
+			if (objects.get(x) instanceof Rectangle2D.Double)
+			{
+				paint.fill((Rectangle2D.Double)objects.get(x));
+			}
+			else if (objects.get(x) instanceof Image)
+			{
+				
+			}
+			else if (objects.get(x) instanceof Button)
+			{
+				
+			}
+		}
+	}
 	
 
 }
