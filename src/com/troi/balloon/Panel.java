c43531c014f0c96426db126abff7c948852c1769
Ryan Chen <ryanjchen@yahoo.com>
2015-04-26 15:24:06 -0700

    oh stuff

--- src/com/troi/balloon/Panel.java
+++ src/com/troi/balloon/Panel.java
@@ -1,26 +1,26 @@
 package com.troi.balloon;
 
 import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.geom.Dimension2D;
 import java.awt.geom.Rectangle2D;
 import java.util.HashMap;
 
 import javax.swing.JFrame;
 
 import util.panelDimension;
-
 public class Panel {
-	
 	protected boolean repaintValue = false;
-	protected HashMap map, buttons;
+	protected HashMap map, buttons; //huh
 	protected int sizeOfMap,x,y,width,hieght;
 	protected Graphics2D paint;
 	protected panelDimension dimension;
 	protected Rectangle2D.Double background;
 	protected String type;
-	
-	public Panel(JFrame frame, String type,Graphics2D paint) {
+	public Panel(JFrame frame, String type,Graphics2D paint)
+	{
 		this.type = type;
-		background = new Rectangle2D.Double(0,0,frame.getHeight(),(frame.getWidth() / 3)); 
+		background = new Rectangle2D.Double(0,0,frame.getHeight(),(frame.getWidth() / 3));
 		repaintValue = true;
 		map = new HashMap();
 		buttons = new HashMap();
@@ -30,44 +30,47 @@ public class Panel {
 		this.paint = paint;
 	}
 	
-	public void paintPanel(Graphics2D paint) {
+	public void paintPanel(Graphics2D paint)
+	{
 		sizeOfMap = buttons.size();
 		paint.fill((Rectangle2D.Double)map.get("background"));
 		for(int x = 0; x < sizeOfMap;x++)
 		{
-			((Button) buttons.get(x)).paint(paint);
+			map.get(buttons.get(x).paint());
 		}
 	}
-	
-	public void setSize(panelDimension size) {
+	public void setSize(panelDimension size)
+	{
 		background.setFrame(size.getX(), size.getY(), size.getWidth(), background.getHeight());
 	}
-	
-	public void repaintButton(Button button) {
-		((Button) buttons.get(button)).paint(paint);
+	public void repaintButton(Button button)
+	{
+		map.get(this.buttons.get(button).paint());
 	}
 
-	public void moveButtonPanel(Panel panel, Button button) {
+	public void moveButtonPanel(Panel panel, Button button)
+	{
 		panel.addButton(button);
 		this.removeButton(button);
 	}
-	
 	public String getType()
 	{
 		return type;
 	}
-	
-	public void removeButton(Button button) {
+	public void removeButton(Button button)
+	{
 		buttons.remove(button);
 	}
-	
-	public panelDimension returnDimension() {
+	public panelDimension returnDimension()
+	{
 		dimension = new panelDimension(x,y,width,hieght);
 		return dimension;
 	}
-	
-	public void addButton(Button button) {
+	public void addButton(Button button)
+	{
 		sizeOfMap += 1;
 		buttons.put(sizeOfMap, button);
 	}
+	
+
 }
