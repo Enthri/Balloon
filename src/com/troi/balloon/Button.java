b06456598c9903f5dfbbd010f0b8b1328aa38ea2
Ryan Chen <ryanjchen@yahoo.com>
2015-04-26 14:22:51 -0700

    updated the button class

--- src/com/troi/balloon/Button.java
+++ src/com/troi/balloon/Button.java
@@ -1,5 +1,45 @@
 package com.troi.balloon;
 
-public class Button {
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+import org.w3c.dom.events.MouseEvent;
+
+public class Button {
+	int startX;
+	int startY;
+	int glo_x;
+	int glo_y;
+	int width;
+	int height;
+	BufferedImage button;
+	public Button(int x, int y, int w, int h){
+		glo_x = x;
+		glo_y = y;
+		w = width;
+		h = height;
+		try{
+			button = ImageIO.read(new File("resources/gray-button-template"));
+		}catch(IOException e){
+			button = null;
+		}
+	}
+	public void paint(Graphics2D render) {
+		render.drawImage(button.getScaledInstance(width, height, Image.SCALE_FAST), glo_x, glo_y, null);
+	}
+	public void move(){
+		
+	}
+	public void MouseDragged(MouseEvent e){
+		
+	}
+	
+	public void resize(){
+		
+	}
 }
