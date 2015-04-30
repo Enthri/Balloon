acb36a029aabbd781506b232f078216bf12e72e3
alexander Patapoff <awpatapoff@gmail.com>
2015-04-29 23:31:38 -0700

    cool

--- src/DragAndDrop/DragAndDrop.java
+++ src/DragAndDrop/DragAndDrop.java
@@ -23,6 +23,7 @@ public class DragAndDrop{
 	{
 		if (currentPanels.contains(new Object()) == true)
 		{
+			
 			if (currentPanels.get(2) instanceof PackageManager)
 			{
 				currentPanels.set(1, new PackageTools(manager.getToolDimension()));
@@ -33,11 +34,11 @@ public class DragAndDrop{
 			}
 			else if (currentPanels.get(2) instanceof MethodManager)
 			{
-				currentPanels.set(1, new PackageTools(manager.getToolDimension()));
+				currentPanels.set(1, new MethodTools(manager.getToolDimension()));
 			}
 			else if (currentPanels.get(2) instanceof CommandManager)
 			{
-				currentPanels.set(1, new PackageTools(manager.getToolDimension()));
+				currentPanels.set(1, new CommandTools(manager.getToolDimension()));
 			}
 			else
 			{
@@ -50,24 +51,25 @@ public class DragAndDrop{
 	{
 		if (currentPanels.contains(new Object()) == true)
 		{
-			if (currentPanels.get(0) instanceof PackageManager)
+			if (currentPanels.get(0) == null)
 			{
-				currentPanels.set(2, new PackageTools(manager.getMainDimension()));
+				currentPanels.set(2, new PackageManager(manager.getMainDimension()));
+				addNewTool();
+			}
+			
+			else if (currentPanels.get(0) instanceof PackageManager)
+			{
+				currentPanels.set(2, new PackageManager(manager.getMainDimension()));
 				addNewTool();
 			}
 			else if (currentPanels.get(0) instanceof ClassManager)
 			{
-				currentPanels.set(2, new PackageTools(manager.getMainDimension()));
+				currentPanels.set(2, new MethodManager(manager.getMainDimension()));
 				addNewTool();
 			}
 			else if (currentPanels.get(0) instanceof MethodManager)
 			{
-				currentPanels.set(2, new PackageTools(manager.getMainDimension()));
-				addNewTool();
-			}
-			else if (currentPanels.get(0) instanceof CommandManager)
-			{
-				currentPanels.set(2, new PackageTools(manager.getMainDimension()));
+				currentPanels.set(2, new CommandManager(manager.getMainDimension()));
 				addNewTool();
 			}
 			else
@@ -89,7 +91,7 @@ public class DragAndDrop{
 	}
 	
 	public void removeCurrentTool()
-	{
+	{1
 		
 	}
 	
