55bebd8c71b09821e7454164b11126f84d7189d3
LevoSpacewars <awpatapoff@gmail.com>
2015-05-04 09:08:58 -0600

    no

--- src/com/troi/balloon/UiManager.java
+++ src/com/troi/balloon/UiManager.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.troi.balloon;
 
 import java.awt.Graphics2D;
@@ -72,78 +72,3 @@ public class UiManager{
 	}
 
 }
-=======
-package com.troi.balloon;
-
-import java.awt.Graphics2D;
-import java.util.ArrayList;
-
-import javax.swing.JFrame;
-
-import util.panelDimension;
-
-
-public class UiManager{
-	panelDimension mainDimension, sideDimension, toolDimension;
-	ArrayList<Panel> panels = new ArrayList<Panel>();
-	JFrame frame;
-	Graphics2D paint;
-	TextEditer textEditor;
-	DragAndDrop guiEditor;
-	GraphicsComponent gComponent;
-	boolean textInUse;
-	
-	public UiManager(JFrame frame)
-	{
-		sideDimension = new panelDimension(0,0,(frame.getWidth() / 5),frame.getHeight());
-		toolDimension = new panelDimension(sideDimension.getWidth(),0,(frame.getWidth() / 5),frame.getHeight());
-		mainDimension = new panelDimension((sideDimension.getWidth() * 2),0,(frame.getWidth() - (sideDimension.getWidth() * 2)), frame.getHeight());
-		gComponent = new GraphicsComponent(); 
-		textEditor = new TextEditer();
-		guiEditor = new DragAndDrop(frame,this);
-		frame.setContentPane(gComponent);
-		Panel panel = new Panel(frame,"main");
-		Panel panel2 = new Panel(frame,"main");
-		gComponent.newPanel(panel);
-		gComponent.newPanel(panel2);
-	}
-	
-	public void addPanel(Panel panel)
-	{
-		
-			panels.add(panel);
-//			if (panel.getType().equals("main"))
-//			{
-//				panel.setSize();
-//			}
-//			if (panel.getType.equal("tools"))
-//			{
-//				
-//			}
-//		
-		
-	}
-	
-	public void movePanelsAround(Panel panel)
-	{
-		panel.setSize(new panelDimension(0, 0, 0, 0));
-	}
-	public void resetEnviroment()
-	{
-		
-	}
-	
-	public void setCurrentEnviroment(Object panel)
-	{
-		if (panel instanceof TextEditer)
-		{
-			textInUse = true;
-		}
-		else if (panel instanceof DragAndDrop)
-		{
-			textInUse = false;
-		}
-	}
-
-}
->>>>>>> origin/master
