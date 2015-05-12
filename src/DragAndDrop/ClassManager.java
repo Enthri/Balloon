8db76fbd9180fc0128166580f217ea796b4c0a75
alexander Patapoff <awpatapoff@gmail.com>
2015-05-11 19:49:26 -0700

    asasd

--- src/DragAndDrop/ClassManager.java
+++ src/DragAndDrop/ClassManager.java
@@ -1,11 +1,14 @@
 package DragAndDrop;
 
+import com.troi.balloon.Button;
+
 import util.panelDimension;
 
 public class ClassManager extends ObjectManager {
 
 	public ClassManager(panelDimension dimension, String state) {
 		super(dimension, state);
+		addNewButton(new Button(this));
 	}
 
 }
