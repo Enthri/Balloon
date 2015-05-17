7791756f85f868b695666a7849afd10cd9c485d2
Troi-Ryan Stoeffler <troiryan2@gmail.com>
2015-05-16 20:32:30 -0700

    code from my old branch

--- src/com/troi/balloon/Balloon.java
+++ src/com/troi/balloon/Balloon.java
@@ -36,3 +36,69 @@ public class Balloon implements Runnable {
 		//hello
 	}
 }
+
+//package com.troi.balloon;
+//
+//import java.awt.Dimension;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//import javax.swing.JFrame;
+//import javax.swing.Timer;
+//
+//import com.troi.balloon.panelsystem.Environment;
+//
+//public class Balloon implements Runnable {
+//	
+//	private JFrame frame;
+//	private Environment currentEnvironment;
+//	
+//	public static void main(String[] args) {
+//		
+//		(new Thread(new Balloon())).start();
+//		
+//	}
+//
+//	@Override
+//	public void run() {
+//		frame = new JFrame("Test");
+//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//		frame.setPreferredSize(new Dimension(800, 600));
+//		frame.pack();
+//		frame.setVisible(true);
+//		Timer timer = new Timer(50, new ActionListener(){
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				currentEnvironment.update();
+//			}
+//		});
+//		timer.start();
+//		this.setCurrentEnvironment(new Environment());
+//		com.troi.balloon.panelsystem.Panel panel = new com.troi.balloon.panelsystem.Panel();
+//		panel.add(new com.troi.balloon.panelsystem.Button());
+//		currentEnvironment.add(panel);
+//		currentEnvironment.add(new com.troi.balloon.panelsystem.Panel());
+//		currentEnvironment.add(new com.troi.balloon.panelsystem.Panel());
+//		currentEnvironment.add(new com.troi.balloon.panelsystem.Panel());
+//		currentEnvironment.add(new com.troi.balloon.panelsystem.Panel());
+////		frame.pack();
+////        final RSyntaxTextArea textArea = new RSyntaxTextArea(20, 60);
+////        textArea.setSyntaxEditingStyle(SyntaxConstants.SYNTAX_STYLE_JAVA);
+////        textArea.setCodeFoldingEnabled(true);
+////        textArea.setText("public class Test {\n}");
+////        RTextScrollPane sp = new RTextScrollPane(textArea);
+////        frame.add(sp);
+////        System.setProperty("java.home", "C:\\Program Files\\Java\\jdk1.7.0_71");
+////        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+//		//hello
+//	}
+//	
+//	public void setCurrentEnvironment(Environment environment) {
+//		currentEnvironment = environment;
+//		frame.setContentPane(currentEnvironment);
+//	}
+//	
+//	public Environment getCurrentEnvironment() {
+//		return currentEnvironment;
+//	}
+//}
