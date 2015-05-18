0409b8a72d2b4e42f7de97137515567254926a4d
Ryan Chen <ryanjchen@yahoo.com>
2015-05-17 20:15:55 -0700

    Comment Coded Block Class

--- src/com/troi/balloon/Block.java
+++ src/com/troi/balloon/Block.java
@@ -2,9 +2,9 @@ package com.troi.balloon;
 
 public class Block extends Button{
 
-	public Block(Panel p) {
+	public Block(Panel p) { //This is a superclass for all Block related classes
 		super(p);
-		setText("Block");
+		setText("Block"); //This allows the Block to be named easily without it being a parameter.
 	}
 
 }
